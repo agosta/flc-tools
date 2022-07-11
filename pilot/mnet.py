033ce0abcb7b3c977e6f219b4e806afc3f80c0d8
Giovanni Agosta <agosta@acm.org>
2022-07-11 17:15:58 +0200

    [Pilot] Pilot automaton according to the FLC course

---
+++ pilot/mnet.py
@@ -0,0 +1,136 @@
+#!/usr/bin/python3
+
+# networkx represents graphs, pydot does conversion to/from graphviz
+import networkx as nx
+import pydot   
+import matplotlib.pyplot as plt
+
+# Basic Graph Definition
+#  axiom machine must be S
+#  machines are labeled with nonterminals, initial states are marked by a transition from an invisible state named init+NonTerm name, states are names NonTerm + number
+#  epsilon moves are labeled "&epsilon;"
+#  the tap is denoted as "-\|"
+
+class Node(object):
+  def __init__(self, name, label=""):
+     self.name=name
+     self.label=label
+     self.to={}
+     self.initial=False
+     self.final=False
+ 
+  def add_arc_to(self,name,label=""):
+     if name not in self.to : self.to[name]=[label]
+     else : self.to[name].append(label)
+
+  def del_arc_to(self,name,label):
+     if len(self.to[name])==1 : self.to.remove(name)
+     elif len(self.to[name])>1 : self.to[name].remove(label)
+
+  def get_arcs(self):
+     return sum([ [ (n, l) for l in self.to[n] ] for n in self.to ],[])
+
+  def __repr__(self):
+     res=' '+self.name
+     if self.final : res+=' [shape="doublecircle"];\n' 
+     else : res+='\n'
+     for s in self.to :
+        for l in self.to[s] :
+          res+=' {} -> {} [label="{}"];\n'.format(self.name,s,l)
+     return res
+
+class Machine(object):
+   def __init__(self, name):
+     self.name=name
+     self.nodes=[]
+
+   def get_initial(self):
+     for n in self.nodes :
+       if n.initial : return n
+
+   def set_initial(self, name):
+     for n in self.nodes :
+       if n.name == name : n.initial=self.name
+     
+   def get_final(self):
+     return [ n for n in self.nodes if n.final ]
+
+   def set_final(self, name):
+     for n in self.nodes :
+       if n.name == name : n.final=self.name
+
+   def add_arc(self, nfrom, nto, label=""):
+     for n in self.nodes :
+       if n.name == nfrom : 
+          n.add_arc_to(nto, label)
+          return
+     #print([ n.name for n in self.nodes ])
+     #print([ nfrom, nto ])
+     raise KeyError
+   
+   def del_arc(self, nfrom, nto):
+     for n in self.nodes :
+       if n.name == nfrom : n.del_arc_to(nto)
+
+   def add_node(self, name, label=""):
+     self.nodes.append(Node(name, label))
+
+   def del_node(self, name):
+     to_remove = [n for n in self.nodes if n.name==name]
+     for n in to_remove:
+       self.nodes.remove(n)
+
+   def get_node(self, name):
+     return [n for n in self.nodes if n.name==name][0]
+
+   def __repr__(self): 
+     res=' init{} [shape="none" style="invis"];\n'.format(self.name)
+     for n in self.nodes: 
+        res+=repr(n)
+     return res + ' init{} -> {}'.format(self.name, self.get_initial().name)
+
+# Importing from Graphviz through networkx
+
+class MachineNet(dict):
+  def __init__(self,filename=None):
+    self.from_dot(filename) if filename else {}
+
+  def from_dot(self,fname="gram0.dot"):
+    G = nx.nx_pydot.read_dot(fname)
+    for c in nx.weakly_connected_components(G):
+      current = None
+      for n in c :
+        if 'init' in n :
+          M = Machine(n[4:])
+          self[n[4:]]=M
+          current=n[4:]
+      M=self[current] 
+      for n in c :
+        if 'init' in n : continue
+        M.add_node(n)
+        try : 
+          if G._node[n]['shape'] == '"doublecircle"' : M.set_final(n)
+        except KeyError : pass
+        for p in G.predecessors(n):
+          if 'init' in p : M.set_initial(n)
+        for s in G.successors(n):
+          for i in range(G.number_of_edges(n,s)):
+            M.add_arc(n,s,eval(G.get_edge_data(n,s,i)['label']))
+
+  def to_dot(self,fname="out.dot"):
+    res = 'digraph G {\n rankdir="LR";\n node [shape="circle"];\n\n'
+    for M in self :
+      res+=repr(self[M])+'\n'
+    res+='}'
+    with open(fname,"w") as fout:
+      fout.write(res)
+
+  def get_node(self, name):
+    for M in self :
+      try : return self[M].get_node(name)
+      except Exception: pass
+    return name
+
+if __name__ == '__main__':
+  G = MachineNet("gram0.dot")
+  G.to_dot()
---
+++ pilot/pilot.py
@@ -0,0 +1,188 @@
+#!/usr/bin/python3
+
+import mnet as mn
+index = 0
+
+class MachineNetPilot(mn.MachineNet):
+  def get_states(self):
+    return sum([ self[m].nodes for m in self ],[])
+
+  def get_state(self,name):
+    '''Fails if name is not the name of a state; also, all names should be different'''
+    return [ n for n in [ self[m].nodes for m in self ] if n.name==name][0]
+      
+  def isNullable(self, state):
+    if state.final : return True
+    for n in state.to : 
+      if state.to[n]=='&epsilon' and self.isNullable(n) : return True
+    return False
+
+  def compute_initials(self):
+    for s in self.get_states():
+      self.initials(s)
+
+  def initials(self, state):
+    #print("Ini({})".format(state.name))
+    edge_labels_term = [ l for n, l in state.get_arcs() if l not in self.keys() ]
+    edge_labels_nonterm = [ l for n, l in state.get_arcs() if l in self.keys() ]
+    edges_and_labels_nonterm = [ (n, l) for n, l in state.get_arcs() if l in self.keys() ]
+    clause1 = set(edge_labels_term)
+    clause2 = set().union(*[ self.initials(self[s].get_initial()) for s in edge_labels_nonterm ])
+    clause3 = set([ self.initials(n) for n, s in edges_and_labels_nonterm if self.isNullable(self[s].get_initial()) ])
+    state.ini = clause1|clause2|clause3
+    return state.ini
+
+  def print_initials(self):
+    print("Initials")
+    for s in self.get_states():
+      print("  Ini("+s.name+'): {',end=" ")
+      for i in s.ini :
+        print(i,end=" ")
+      print('}')
+
+class Item(object):
+  def __init__(self,state,la):
+    self.state=state
+    self.la=la
+
+  def __hash__(self):
+    return hash((self.state.name, self.la))
+
+  def __eq__(self, other):
+    if self.state.name==other.state.name and self.la == other.la : return True
+    return False
+
+  def __ne__(self, other):
+    return not self.__eq__(other)
+
+  def __lt__(self, other):
+    if self.state.name<other.state.name : return 1
+    if self.state.name==other.state.name and self.la < other.la : return 1
+    return 0
+
+  def __repr__(self):
+     name = self.state.name if not self.state.final else '({})'.format(self.state.name)
+     return "{}, {}".format(name,self.la)
+
+class MacroState(mn.Node):
+  def __init__(self,base, machine_net):
+    self.base=set(base)
+    self.mn=machine_net
+    self.closure=set()
+    self.get_closure()
+    global index
+    self.name="I{}".format(index)
+    index+=1
+    self.label=self.name
+    self.to={}
+    self.initial=False
+    self.final=False
+
+  def __hash__(self):
+    return hash((tuple(sorted(list(self.base))), tuple(sorted(list(self.closure)))))
+
+  def __eq__(self, other):
+    return hash(self)==hash(other)
+
+  def __ne__(self, other):
+    return hash(self)!=hash(other)
+
+  def __repr__(self):
+    base = '\l'.join([ str(i) for i in self.base])
+    clos = '\l'.join([ str(i) for i in self.closure])
+    res = ' ' +self.name + ' [xlabel="{}" label="'.format(self.name) + base +"|"+ (clos if len(self.closure) else " ") +'" ];\n'
+    for s in self.to :
+      for l in self.to[s] :
+        res+=' {} -> {} [label="{}"];\n'.format(self.name,s,l)
+    return res
+
+  def get_closure(self):
+    for item in self.base:
+      state=item.state
+      la=item.la
+      edges_and_labels_nonterm = [ (n, l) for n, l in self.mn.get_node(state).get_arcs() if l in self.mn.keys() ]
+      for n, l in edges_and_labels_nonterm :
+        n=self.mn.get_node(n)
+        if self.mn.isNullable(n) :
+          self.closure.add(Item(self.mn[l].get_initial(), la))
+        else :
+          for b in n.ini:
+            self.closure.add(Item(self.mn[l].get_initial(), b))
+    length=len(self.closure)
+    while True :
+      for item in self.closure :
+        state=item.state
+        la=item.la
+        edges_and_labels_nonterm = [ (n, l) for n, l in state.get_arcs() if l in self.mn.keys() ]
+        for n, l in edges_and_labels_nonterm :
+          n=self.get_node(n)
+          if self.isNullable(n) :
+            self.closure.add(Item(self.mn[l].get_initial(), la))
+          else :
+            for b in n.ini():
+              self.closure.add(Item(self.mn[l].get_initial(), b))
+      if len(self.closure)>length : length=len(self.closure)
+      else : return
+
+  def get_nodes(self):
+    #print(self.base|self.closure)
+    return self.base|self.closure
+
+class Pilot(mn.Machine):
+   def __init__(self, machine_net):
+     self.name='pilot'
+     self.mn=machine_net
+     self.nodes=[]
+     initial_state=MacroState([Item(self.mn['S'].get_initial(),'-\|')],self.mn)
+     self.nodes.append(initial_state)
+     self.set_initial(initial_state.name)
+     self.built=False
+     self.built=self.build()
+
+   def update(self,l):
+     '''Perform the pilot automaton building step, adding new macro-states'''
+     # for each mstate and symbol in alphabet, for which we have a transition (i.e., compute all possible shifts)
+     for I in self.nodes[l:] :
+       shifts = sum([ [ (item.state, to, label, item.la) for to, label in item.state.get_arcs() ] for item in I.get_nodes() ],[])
+       labels = set([ shift[2] for shift in shifts ]) # set of all outgoing labels
+       # collect the shifts by label, and build the closure of each macro-state (the latter is done by the macro-state constructor)
+       for l in labels :
+         new_state=MacroState([ Item(self.mn.get_node(shift[1]), shift[3]) for shift in shifts if shift[2]==l ],self.mn)
+         # check if it is a new state, then add it, otherwise add just the arc, to the old state.
+         if new_state not in self.nodes:
+           self.nodes.append(new_state)
+           name=new_state.name
+         else :
+            for old_state in self.nodes: 
+              if new_state == old_state : 
+                name=old_state.name
+         self.add_arc(I.name,name,l)
+                   
+   def build(self):
+     '''Perform the pilot automaton building loop, up to fixed point'''
+     if self.built : return True
+     lold=0
+     l=len(self.nodes)
+     while True:
+       self.update(lold)
+       lold=l
+       print(self)
+       if len(self.nodes)>l and l<9 : l=len(self.nodes)
+       else : return True
+
+   def __repr__(self): 
+     res='digraph P {{\nrankdir="LR";\nnode [shape="record"];\n init{} [shape="none" style="invis"];\n'.format(self.name)
+     for n in self.nodes: 
+        res+=repr(n)
+     return res + ' init{} -> {}'.format(self.name, self.get_initial().name)+'\n}'
+  
+
+if __name__ == '__main__':
+  M = MachineNetPilot("gram0.dot")
+  M.compute_initials()
+  M.print_initials()
+  pilot=Pilot(M)
+  pilot.build()
+  with open("pilot.dot","w") as fout:
+    fout.write(str(pilot))
+
